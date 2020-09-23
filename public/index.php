99ba3e7a92b0b0647437c643477024ccf59bb826
joaomoraski <joaomoraskilunkes@gmail.com>
2020-09-23 16:47:28 -0300

    Terminando classe model

--- public/index.php
+++ public/index.php
@@ -1,11 +1,20 @@
 <?php
-require_once(dirname(__FILE__,2) . '/src/config/database.php'); // se colocar o 2 ele pega o diretorio pai
+require_once(dirname(__FILE__,2) . '/src/config/config.php'); // se colocar o 2 ele pega o diretorio pai
+require_once(dirname(__FILE__,2) . '/src/models/User.php');
 
+//$user = new User(['name' => 'Lucas', 'email' => 'lucas@gmail.com']);
+//print_r($user);
+//$user->email = 'lucas_alterador@gmail.com';
+//print_r($user->email);
+//echo '<br>Fim';
+//echo $user->getSelect();
 
-$sql = 'select * from users';
-$result = Database::getResultFromQuery($sql);
+//echo User::getSelect(['id' => 1], 'name, email');
+print_r(User::get(['id' => 1], 'name, email'));
+echo '<br>';
+//echo User::getSelect(['name' => 'Chaves']);
 
-while($row = $result->fetch_assoc()){
-    print_r($row);
+foreach (User::get([], 'name') as $user){
+    echo $user->name;
     echo '<br>';
 }
