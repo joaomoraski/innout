9d4e9449aa706ba44eea661e703314f829307348
joaomoraski <joaomoraskilunkes@gmail.com>
2020-09-23 16:48:45 -0300

    Terminando classe model

--- public/index.php
+++ public/index.php
@@ -3,16 +3,8 @@ require_once(dirname(__FILE__,2) . '/src/config/config.php'); // se colocar o 2 
 require_once(dirname(__FILE__,2) . '/src/models/User.php');
 
 //$user = new User(['name' => 'Lucas', 'email' => 'lucas@gmail.com']);
-//print_r($user);
-//$user->email = 'lucas_alterador@gmail.com';
-//print_r($user->email);
-//echo '<br>Fim';
-//echo $user->getSelect();
-
-//echo User::getSelect(['id' => 1], 'name, email');
 print_r(User::get(['id' => 1], 'name, email'));
 echo '<br>';
-//echo User::getSelect(['name' => 'Chaves']);
 
 foreach (User::get([], 'name') as $user){
     echo $user->name;
